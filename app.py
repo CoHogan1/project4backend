f083f330b8c92d1d66e51163b744a60bb2e66144
Conner Hogan <ConnerRHogan@gmail.com>
2021-06-08 17:40:01 -0600

    CORS *

--- app.py
+++ app.py
@@ -38,12 +38,12 @@ def load_user(user_id):
 
 #socketIO
 if os.environ.get("FLASK_ENV") == 'development':
-    socketio = SocketIO(app, cors_allowed_origins="https://localhost:3000")
-    CORS(users, origins=['https://localhost:3000'], supports_credentials=True)
+    socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
+    CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
 
 else:
-    socketio = SocketIO(app, cors_allowed_origins="https://front-end-444.herokuapp.com") # previous ex
-    #socketio = SocketIO(app, cors_allowed_origins="*")
+    #socketio = SocketIO(app, cors_allowed_origins="https://front-end-444.herokuapp.com") # previous ex
+    socketio = SocketIO(app, cors_allowed_origins="*")
     CORS(users, origins=['https://front-end-444.herokuapp.com'], supports_credentials=True)
 
 
