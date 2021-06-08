f08f01c5b2ec9ee768549e7d976b769503d586cd
Conner Hogan <ConnerRHogan@gmail.com>
2021-06-08 16:51:18 -0600

    removed star

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ if os.environ.get("FLASK_ENV") == 'development':
     CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
 
 else:
-    #socketio = SocketIO(app, cors_allowed_origins="https://front-end-444.herokuapp.com") # previous ex
-    socketio = SocketIO(app, cors_allowed_origins="*")
+    socketio = SocketIO(app, cors_allowed_origins="https://front-end-444.herokuapp.com") # previous ex
+    #socketio = SocketIO(app, cors_allowed_origins="*")
     CORS(users, origins=['https://front-end-444.herokuapp.com'], supports_credentials=True)
 
 
